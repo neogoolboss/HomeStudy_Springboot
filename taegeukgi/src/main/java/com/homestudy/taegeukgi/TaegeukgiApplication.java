92c2c67c38e21bf77235b85f2a5b1bc30b01df33
neogoolboss <krossjsnet88@gmail.com>
2024-04-21 22:15:46 +0900

    2024 04 21

---
+++ taegeukgi/src/main/java/com/homestudy/taegeukgi/TaegeukgiApplication.java
@@ -0,0 +1,13 @@
+package com.homestudy.taegeukgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaegeukgiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaegeukgiApplication.class, args);
+    }
+
+}
---
+++ taegeukgi/src/test/java/com/homestudy/taegeukgi/TaegeukgiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homestudy.taegeukgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaegeukgiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
